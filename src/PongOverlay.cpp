21c8f7d403fece1c7155dc5214b4859907475538
Alex Shaw <github@glastonbridge.co.uk>
2013-01-30 08:07:06 +0000

    Moar pong!

--- src/PongOverlay.cpp
+++ src/PongOverlay.cpp
@@ -7,3 +7,78 @@
 //
 
 #include "PongOverlay.h"
+
+#include "PaddleSensor.h"
+
+const int paddleWidth = 10;
+const int paddleHeight = 30;
+const int paddlePadding = 5;
+
+const std::string PongOverlay::NAME = "pong overlay";
+
+std::string PongOverlay::getName()
+{
+    return NAME;
+}
+
+void PongOverlay::setup(float width, float height)
+{
+    _width = width; _height = height;
+    _ballVel.x = 4;
+    _ballVel.y = 4;
+    resetBall();
+}
+
+void PongOverlay::resetBall()
+{
+    _ballPos.x = _width/2;
+    _ballPos.y = _height/2;
+}
+
+void PongOverlay::update(ofxCvColorImage *input)
+{
+    PaddleSensor* paddles = dynamic_cast<PaddleSensor*> (sensors[0]);
+    if (_ballPos.y>_height || _ballPos.y<0) _ballVel.y = -_ballVel.y;
+    
+    if (_ballVel.x > 0) // ball travelling left-to-right
+    {
+        if (_ballPos.x + _ballVel.x > _width-paddleWidth-paddlePadding) // will we go past the bat?
+        {
+            if (_ballPos.y > paddles->rightPaddle && _ballPos.y < paddles->rightPaddle + paddleHeight)
+            {
+                _ballVel.x = - _ballVel.x;
+            }
+            else
+            {
+                resetBall();
+            }
+        }
+    }
+    else // ball travelling right-to-left
+    {
+        if (_ballPos.x + _ballVel.x < paddleWidth + paddlePadding) // will we go past the bat?
+        {
+            if (_ballPos.y > paddles->leftPaddle && _ballPos.y < paddles->leftPaddle + paddleHeight)
+            {
+                _ballVel.x = - _ballVel.x;
+            }
+            else
+            {
+                resetBall();
+            }
+        }
+    }
+    
+    _ballPos += _ballVel;
+}
+
+void PongOverlay::draw()
+{
+    PaddleSensor* paddles = dynamic_cast<PaddleSensor*> (sensors[0]);
+    ofColor(0xFFFFFF);
+    
+    ofRect(paddlePadding, paddles->leftPaddle, paddleWidth , paddleHeight);
+    ofRect(_width-paddleWidth-paddlePadding, paddles->rightPaddle, paddleWidth, paddleHeight);
+    
+    ofCircle(_ballPos, paddleWidth/2);
+}
--- src/PongOverlay.h
+++ src/PongOverlay.h
@@ -9,6 +9,24 @@
 #ifndef fullSpectrumAnalyser_PongOverlay_h
 #define fullSpectrumAnalyser_PongOverlay_h
 
+#include "Overlay.h"
 
+class PongOverlay : public Overlay
+{
+public:
+    static const std::string NAME;
+    std::string getName();
+    
+    virtual void update(ofxCvColorImage* input);
+    virtual void draw();
+    virtual void setup(float width, float height);
+    
+private:
+    ofPoint _ballPos;
+    ofPoint _ballVel;
+    float _width, _height;
+    
+    void resetBall();
+};
 
 #endif
